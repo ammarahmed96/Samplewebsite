ccd05843d46fb571357db9f2cdd7898b70102226
Ammar Ahmed <ammarahmed12321@gmail.com>
2018-08-10 17:46:40 +0500

    updated Indexjs for uploading images

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let express = require("express");
 let app = express();
 let path = require("path");
-
-//app.set('views', './pages');
-//app.set('view engine', 'ejs');
+let serveStatic = require("serve-static");
 
 app.get("/",function(req, res){
     res.sendFile(path.join(__dirname+'/pages/test.html'))
 });
 
-app.use(express.static('public'));
+app.get("/image1",function(req, res){
+    res.sendFile(path.join(__dirname+'/pages/picbank/pic_320x240.jpg'))
+});
+
+// app.use(express.static('public'));
+
+app.use(serveStatic(path.join(__dirname,'public')));
 
 app.listen(3000, function(){
     console.log("Server is started on port 3000");
